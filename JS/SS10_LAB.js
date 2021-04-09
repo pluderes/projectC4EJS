09893412117c4506c70588aa642bf00dd75a22e4
pluderes <kingofpoppro@gmail.com>
2021-04-09 19:43:29 +0700

    upload

---
+++ JS/SS10_LAB.js
@@ -0,0 +1,60 @@
+async function getDataAPI() {
+  let promise = await fetch(`https://sheetdb.io/api/v1/uo37nyuvbu1k8`);
+  let data = await promise.json();
+  console.log(data);
+
+  let thumb = document.getElementById(`imgFood`);
+  let name = document.getElementById(`name`);
+  let introduce = document.getElementById(`introduce`);
+  let address = document.getElementById(`address`);
+  let review = document.getElementById(`review`);
+  // let link = document.getElementById(`link`);
+  // let ingred = document.getElementById(`listIngred`);
+
+  let size = Object.keys(data).length;
+  console.log(size);
+
+  let index = Math.floor(Math.random() * size+1);
+
+  thumb.src = data[index].Image;
+  name.innerHTML += `${data[index].Name}`;
+  address.innerHTML += `<p style="font-weight: bold;">Address: </p> ${data[index].Address}`;
+  introduce.innerHTML += `<p style="font-weight: bold;">Instructions: </p> ${data[index].Introduce}`;
+  review.innerHTML += `<p style="font-weight: bold;">Review: <a href="${data[index].Review}">${data[index].Review}</a></p>`;
+  //------------------------------------------------------------
+  // let food = data.meals[0];
+
+  // let thumb = document.getElementById(`imgFood`);
+  // let meal = document.getElementById(`meal`);
+  // let cate = document.getElementById(`category`);
+  // let area = document.getElementById(`area`);
+  // let tag = document.getElementById(`tags`);
+  // let instruc = document.getElementById(`instructions`);
+  // let link = document.getElementById(`link`);
+  // let ingred = document.getElementById(`listIngred`);
+
+  // thumb.src = food.strMealThumb;
+  // meal.innerHTML += `${food.strMeal}`;
+  // cate.innerHTML += `<p style="font-weight: bold;">Category:  ${food.strCategory}</p>`;
+  // area.innerHTML += `<p style="font-weight: bold;">Area: ${food.strArea}</p> `;
+  // tag.innerHTML += `<p style="font-weight: bold;">Tags:  ${food.strTags}</p>`;
+  // instruc.innerHTML += `<p style="font-weight: bold;">Instructions: </p> ${food.strInstructions}`;
+  // link.src = convertToEmbedUrl(food.strYoutube);
+
+  //   function convertToEmbedUrl(youtubeUrl) {
+  //     return youtubeUrl.replace("watch?v=", "embed/");
+  // }
+
+  // let size = Object.keys(food).length;
+  // console.log(size);
+
+  // for(let i=1;i<size;i++){
+  //     if()
+  // }
+}
+getDataAPI();
+
+let Reload = document.getElementById('reload');
+Reload.onclick = function () {
+  location.reload();
+}
---
+++ JS/viewmore.js
@@ -0,0 +1,4 @@
+let btnView = document.getElementById(`viewmore`);
+btnView.onclick = function () {
+    location.replace('http://127.0.0.1:5500/SS10_LAB.html');
+};
